bb48af43735a66c90ce090aad40eb85710bfac60
Selcuk Erol <selcuk_erol@hotmail.com>
2020-01-16 19:17:49 +0300

    Edited V2.1

--- app.rb
+++ app.rb
@@ -28,30 +28,6 @@ get '/hakkimizda' do
 
 end
 
-get '/haber1' do
-
-   erb :haber1    
-
-end
-
-get '/haber2' do
-
-   erb :haber2    
-
-end
-
-get '/haber3' do
-
-   erb :haber3    
-
-end
-
-get '/haber4' do
-
-   erb :haber4    
-
-end
-
 get '/sartlar' do
 
    erb :sartlar    
@@ -71,39 +47,9 @@ get '/urunler/skoc' do
 
 end
 
-get '/urunler/biokutle' do
-
-   erb :"urunler/biokutle"     
-
-end
-
-get '/urunler/akiskan' do
-
-   erb :"urunler/akiskan"     
-
-end
-
-get '/urunler/sicaksu' do
-
-   erb :"urunler/sicaksu"     
-
-end
-
 get '/urunler/termal' do
 
    erb :"urunler/termal"     
-
-end
-
-get '/urunler/isikaybi' do
-
-   erb :"urunler/isikaybi"     
-
-end
-
-get '/urunler/suborusu' do
-
-   erb :"urunler/suborusu"     
 
 end
 
@@ -146,7 +92,7 @@ post '/iletisim' do
     puts response.headers
 
     params.inspect
-    flash[:confirm] = "We received your email, we will get back to you shortly"	
+    flash[:confirm] = "Emailınız bize ulaşmıştır en yakın zamanda sizinle iletişime geçicez"	
     redirect '/iletisim'   
 
 end	
@@ -169,30 +115,6 @@ get '/en/aboutus' do
 
 end
 
-get '/en/news1' do
-
-   erb :"en/news1"    
-
-end
-
-get '/en/news2' do
-
-   erb :"en/news2"    
-
-end
-
-get '/en/news3' do
-
-   erb :"en/news3"    
-
-end
-
-get '/en/news4' do
-
-   erb :"en/news4"    
-
-end
-
 get '/en/terms' do
 
    erb :"en/terms"    
@@ -212,39 +134,9 @@ get '/en/products/scotch' do
 
 end
 
-get '/en/products/biomass' do
-
-   erb :"en/products/biomass"     
-
-end
-
-get '/en/products/fluidized' do
-
-   erb :"en/products/fluidized"     
-
-end
-
-get '/en/products/hotwater' do
-
-   erb :"en/products/hotwater"     
-
-end
-
 get '/en/products/thermal' do
 
    erb :"en/products/thermal"     
-
-end
-
-get '/en/products/wasteheat' do
-
-   erb :"en/products/wasteheat"     
-
-end
-
-get '/en/products/waterpipe' do
-
-   erb :"en/products/waterpipe"     
 
 end
 
@@ -260,6 +152,38 @@ get '/en/contactus' do
 
 end
 
+post '/en/contactus' do
+
+    p params
+    p params[:email], params["email"]
+
+    from = SendGrid::Email.new email: params[:email]
+    subject = "Mail from #{params[:name]} #{params[:surname]} (#{params[:company]})"
+    to = SendGrid::Email.new email: "selcuk_erol@hotmail.com"
+
+    content = SendGrid::Content.new(
+        type: 'text/plain',
+        value: params[:comment] 
+        )
+
+    mail = SendGrid::Mail.new(from,subject,to,content)
+
+    p mail
+
+    sg = SendGrid::API.new( api_key: ENV['SENDGRID_API_KEY'])
+
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+    puts response.status_code
+    puts response.body
+    puts response.headers
+
+    params.inspect
+    flash[:confirm] = "We received your email, we will get back to you shortly" 
+    redirect '/en/contactus'   
+
+end
+
 ###########==================================================================================##############
 
                                   ###### RUSSIAN SITE REQUESTS #####
@@ -268,103 +192,49 @@ end
 
 get '/ru/' do
 
-  erb :"ru/home"    
+  erb :"ru/dom"    
 
 end
 
 get '/ru/aboutus' do
 
-   erb :"ru/aboutus"    
-
-end
-
-get '/ru/news1' do
-
-   erb :"ru/news1"    
-
-end
-
-get '/ru/news2' do
-
-   erb :"ru/news2"    
-
-end
-
-get '/ru/news3' do
-
-   erb :"ru/news3"    
-
-end
-
-get '/ru/news4' do
-
-   erb :"ru/news4"    
+   erb :"ru/naschetnas"    
 
 end
 
 get '/ru/terms' do
 
-   erb :"en/terms"    
+   erb :"ru/terms"    
 
 end
 
 
-get '/ru/products' do
+get '/ru/produkt' do
 
-   erb :"ru/productru"     
-
-end
-
-get '/ru/products/scotch' do
-
-   erb :"ru/products/scotch"     
+   erb :"ru/produkt"     
 
 end
 
-get '/ru/products/biomass' do
+get '/ru/produkt/skoc' do
 
-   erb :"ru/products/biomass"     
-
-end
-
-get '/ru/products/fluidized' do
-
-   erb :"ru/products/fluidized"     
+   erb :"ru/produkt/skoc"     
 
 end
 
-get '/ru/products/hotwater' do
+get '/ru/produkt/termal' do
 
-   erb :"ru/products/hotwater"     
-
-end
-
-get '/ru/products/thermal' do
-
-   erb :"ru/products/thermal"     
-
-end
-
-get '/ru/products/wasteheat' do
-
-   erb :"ru/products/wasteheat"     
-
-end
-
-get '/ru/products/waterpipe' do
-
-   erb :"ru/products/waterpipe"     
+   erb :"ru/produkt/termal"     
 
 end
 
 get '/ru/references' do
 
-   erb :"ru/references"
+   erb :"ru/ssylka"
 
 end  
 
 get '/ru/contactus' do
 
-   erb :"ru/contactus"    
+   erb :"ru/Svyazatsyasnami"    
 
 end
